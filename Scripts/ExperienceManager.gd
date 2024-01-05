b9810bdd5268b3516c7003053445a7c8110585de
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2024-01-04 23:37:54 -0800

    Edited

---
+++ Scripts/ExperienceManager.gd
@@ -0,0 +1,14 @@
+extends Control
+
+var currentExp : int
+var maxExp : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	currentExp = maxExp
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
